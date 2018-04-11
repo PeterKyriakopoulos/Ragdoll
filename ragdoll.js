c751badf8ec5840dc86f853924809eefe11d4d93
Pete <petekyriakopoulos@gmail.com>
2018-04-11 23:03:11 +0200

    Add files via upload

--- ragdoll.js
+++ ragdoll.js
@@ -1,4 +1,9 @@
 
+
+//Simulating a ragdoll falling down using Verlet integration. The idea behind Verlet integration is that it uses an objects
+//previous position to calculate its next velocity. This can be accomplished multipled times per frame and is a fast and more
+//stable method, compared to using Euler.
+
 window.onload = function() {
 	var canvas = document.getElementById("canvas"),
 		context = canvas.getContext("2d"),
@@ -8,21 +13,26 @@ window.onload = function() {
 
 	var points = [],
 		limbs = [];
-		gravity = 0.25,
-		bounce = 0.9,
-		friction = 0.999;
+		//grav
+		grav = 0.25,
+		//how "springy" the ragdoll is in terms of bouncing from the walls
+		//changing this value to over 1.5 "breaks" our ragdoll :(
+		rebound = .85,
+		//coefficient of friction
+		//decreasing the value to under .9 causes the ragdoll to slow down due to vertical friction on the walls
+		coefff = 0.99;
 
 		//points represent the connecting points between the lines that construct the body
 		//some of the points will therefore act as the joints in our ragdoll
-		//ramdomizing initial velocity
+		//randomizing initial velocity
 	points.push({
-		x: 90,
+		x: 120,
 		y: 100,
-		oldx: 90 + Math.random() * 50,
+		oldx: 100 + Math.random() * 50,
 		oldy: 100
 	});
 	points.push({
-		x: 210,
+		x: 180,
 		y: 100,
 		oldx: 210,
 		oldy: 100
@@ -39,7 +49,8 @@ window.onload = function() {
 		oldx: 100,
 		oldy: 300
 	});
-	// arms
+	
+	// ARMS--------------------------------------------------------------------------------------------
 	points.push({
 		x: 10,
 		y: 170,
@@ -52,7 +63,8 @@ window.onload = function() {
 		oldx: 290,
 		oldy: 170
 	});
-	// legs
+	
+	// LEGS--------------------------------------------------------------------------------------------
 	points.push({
 		x: 70,
 		y: 400,
@@ -65,7 +77,8 @@ window.onload = function() {
 		oldx: 230,
 		oldy: 400
 	});
-	// hands
+	
+	// FOREARMS----------------------------------------------------------------------------------------
 	points.push({
 		x: 10,
 		y: 230,
@@ -78,6 +91,8 @@ window.onload = function() {
 		oldx: 290,
 		oldy: 230
 	});
+	
+	//KNEE--------------------------------------------------------------------------------------------
 	points.push({
 		x: 70,
 		y: 500,
@@ -104,6 +119,8 @@ window.onload = function() {
 		oldy: 300,
 		pinned: true
 	});
+	
+	//WALL--------------------------------------------------------------------------------------------
 	points.push({
 		x: 250,
 		y: 400,
@@ -121,6 +138,7 @@ window.onload = function() {
 
 	//lines used to connect the dots, and give our ragdoll its shape
 	//invisible lines are used to prevent the body from collapsing into itself
+	//those are set to visible: false
 	
 	limbs.push({
 		p0: points[0],
@@ -166,7 +184,10 @@ window.onload = function() {
 		visible: true
 	});
 	
-	// legs
+	
+	
+	// LEGS--------------------------------------------------------------------------------------------
+	
 	//left
 	limbs.push({
 		p0: points[3],
@@ -186,6 +207,7 @@ window.onload = function() {
 		length: distance(points[6], points[2]),
 		visible: false
 	});
+	
 	//right
 	limbs.push({
 		p0: points[2],
@@ -207,7 +229,9 @@ window.onload = function() {
 		visible: false
 	});
 
-	// arms
+	
+	// ARMS--------------------------------------------------------------------------------------------
+	
 	//left
 	limbs.push({
 		p0: points[4],
@@ -215,6 +239,7 @@ window.onload = function() {
 		length: distance(points[4], points[8]),
 		visible: true
 	});
+	
 	//right
 	limbs.push({
 		p0: points[5],
@@ -223,7 +248,9 @@ window.onload = function() {
 		visible: true
 	});
 	
-	//wall
+	
+	//WALL--------------------------------------------------------------------------------------------
+	
 	limbs.push({
 		p0: points[14],
 		p1: points[15],
@@ -240,12 +267,14 @@ window.onload = function() {
 		return Math.sqrt(dx * dx + dy * dy);
 	}
 
-
+	//-----------------------------------------------------------------------------------------------------------
+	//UPDATE
+	
 	update();
 
 	function update() {
 		updatePoints();
-
+		//note that the points are being updated twice per frame
 		for(var i = 0; i < 2; i++) {
 			updatelimbs();
 			constrainPoints();
@@ -258,58 +287,75 @@ window.onload = function() {
 		for(var i = 0; i < points.length; i++) {
 			var p = points[i], vx, vy
 			if(!p.pinned){
-					vx = (p.x - p.oldx) * friction;
-					vy = (p.y - p.oldy) * friction;
+				//calculating velocity and updating it by setting the variables correctly
+					vx = (p.x - p.oldx) * coefff;
+					vy = (p.y - p.oldy) * coefff;
 				p.oldx = p.x;
 				p.oldy = p.y;
 				p.x += vx;
 				p.y += vy;
-				p.y += gravity;
+				p.y += grav;
 			}
 		}
 	}
-
+	
+	
+	//CONSTRAINTS
+	//used not only to prevent the ragdoll from falling off the screen but also for creating a "tunnel" through which it 
+	//initially falls in the scene. This is done by inversing its velocity
+	
 	function constrainPoints() {
 
 		for(var i = 0; i < points.length; i++) {
 			var p = points[i];
 			if(!p.pinned){
+				
+				//screen border-----------------------------------------------------------------------------
 				if(p.x > width) {
 					p.x = width;
-					p.oldx = p.x + (p.x - p.oldx) * bounce;
+					p.oldx = p.x + (p.x - p.oldx) * rebound;
 				}
 				else if(p.x < 0) {
 					p.x = 0;
-					p.oldx = p.x + (p.x - p.oldx) * bounce;
+					p.oldx = p.x + (p.x - p.oldx) * rebound;
 				}
 				if(p.y > height) {
 					p.y = height;
-					p.oldy = p.y + (p.y - p.oldy) * bounce;
+					p.oldy = p.y + (p.y - p.oldy) * rebound;
 				}
 				if(p.y < 0) {
 					p.y = 0;
-					p.oldy = p.y + (p.y - p.oldy) * bounce;
+					p.oldy = p.y + (p.y - p.oldy) * rebound;
 				}
+				
+				//"tunnel"-----------------------------------------------------------------------------------
 				if((p.y < 400) && (p.x > 250)) {
 					(p.y < 400) && (p.x > 250);
-					p.oldy = p.y + (p.y - p.oldy) * bounce;
+					p.oldy = p.y + (p.y - p.oldy) * rebound;
 				}
 			}
 		}
 	}
-
+	
+	//UPDATING LIMBS--------------------------------------------------------------------------------------------
 	function updatelimbs() {
 		for(var i = 0; i < limbs.length; i++) {
 			var s = limbs[i],
+			//calculating the difference using trigonometry
 				dx = s.p1.x - s.p0.x,
 				dy = s.p1.y - s.p0.y,
-				dist = Math.sqrt(dx * dx + dy * dy);
-				ratio = s.length / dist,
+				dis = Math.sqrt(dx * dx + dy * dy);
+				//using the ratio of length over total distance
+				ratio = s.length / dis,
+				//making each point travel half the distance (x and y)
 				midx = s.p0.x + dx / 2,
 				midy = s.p0.y + dy / 2,
+				
+				//calculating the offset
 				offsetx = dx / 2 * ratio,
 				offsety = dy / 2 * ratio;
-
+			
+			//subtracting and adding offsets
 			s.p0.x = midx - offsetx;
 			s.p0.y = midy - offsety;
 			s.p1.x = midx + offsetx;
@@ -317,7 +363,10 @@ window.onload = function() {
 	
 		}
 	}
-
+	
+	
+	
+	//RENDER--------------------------------------------------------------------------------------------
 	function render() {
 		context.clearRect(0, 0, width, height);
 		for(var i = 0; i < limbs.length; i++) {
